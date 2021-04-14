9cb4866f9ff9b19dcbea0d67cf63a144539b960a
jeremiahalde <82428197+jeremiahalde@users.noreply.github.com>
2021-04-14 23:26:21 +0800

    exam code
    exam code

---
+++ exam/exam/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace exam
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(521, 12);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 0;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Email";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(67, 12);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(169, 23);
+            this.txtUser.TabIndex = 2;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(305, 12);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(191, 23);
+            this.txtPass.TabIndex = 3;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(25, 55);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(571, 169);
+            this.txtResult.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(242, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Password";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 248);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLogin);
+            this.Name = "Form1";
+            this.Text = "Exam";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ exam/exam/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net;
+using Cookie = System.Net.Cookie;
+using System.IO;
+using HtmlAgilityPack;
+using System.Web;
+using HtmlDocument = HtmlAgilityPack.HtmlDocument;
+using Newtonsoft.Json;
+
+namespace exam
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        private async void btnLogin_Click(object sender, EventArgs e)
+        {
+            string url = "https://github.com/login";
+            string username = txtUser.Text;
+            string password = txtPass.Text;
+
+            HtmlAgilityPack.HtmlWeb web = new HtmlWeb();
+            HtmlAgilityPack.HtmlDocument doc = web.Load(url);
+
+            var token = doc.DocumentNode.SelectNodes("/html/body/div[3]/main/div/div[4]/form/input[1]");
+            string token_val = token[0].Attributes[2].Value;
+            using var client = new HttpClient();
+
+            CookieContainer cookieContainer = new CookieContainer();
+            var req = (HttpWebRequest)WebRequest.Create(url);
+            req.CookieContainer = cookieContainer;
+            req.Method = "GET";
+
+            WebResponse resp = req.GetResponse();
+
+            HttpWebResponse response1 = resp as HttpWebResponse;
+
+            CookieCollection cookies;
+            cookies = response1.Cookies;
+
+            var a = cookies;
+
+            var baseAddress = new Uri(url);
+            var cookieContainer1 = new CookieContainer();
+            using (var handler = new HttpClientHandler() { CookieContainer = cookieContainer })
+            using (var client1 = new HttpClient(handler) { BaseAddress = baseAddress })
+            {
+                var content = new FormUrlEncodedContent(new[]
+                {
+                 new KeyValuePair<string, string>("login", HttpUtility.UrlEncode(username, Encoding.UTF8)),
+                 new KeyValuePair<string, string>("password", HttpUtility.UrlEncode(password, Encoding.UTF8)),
+                 new KeyValuePair<string, string>("authenticity_token", HttpUtility.UrlEncode(token_val, Encoding.UTF8)),
+                 new KeyValuePair<string, string>("commit", HttpUtility.UrlEncode("Sign in", Encoding.UTF8))
+                });
+                cookieContainer1.Add(baseAddress, new Cookie("_device_id", a[0].Value ));
+                cookieContainer1.Add(baseAddress, new Cookie("_gh_sess", a[1].Value));
+                cookieContainer1.Add(baseAddress, new Cookie("_octo", a[2].Value));
+
+                client1.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",token_val);
+                var authValue = new System.Net.Http.Headers.ProductInfoHeaderValue(username,"");
+                client1.DefaultRequestHeaders.UserAgent.Add(authValue);
+                var result = await client1.PostAsync("/settings/security-log", content);
+                string con = result.Content.ReadAsStringAsync().Result;
+                result.EnsureSuccessStatusCode();
+
+
+                var htmlDoc = new HtmlDocument();
+                htmlDoc.LoadHtml(con);
+
+                var acc = htmlDoc.DocumentNode.SelectNodes("/html/body/div[4]/main/div/div[1]/div/div/h1/a");
+
+
+                jsonData data = new jsonData();
+                data.account = acc[0].InnerText.ToString().Trim();
+              
+                foreach (HtmlNode node in htmlDoc.DocumentNode.SelectNodes("/html/body/div[4]/main/div/div[2]/div[2]/div/div[2]"))
+                {
+                    logs logs = new logs();
+                    logs.events = node.ChildNodes[3].ChildNodes[3].InnerText.ToString();
+                    logs.time = node.ChildNodes[5].ChildNodes[3].InnerText.ToString();
+                }
+
+                string strResultJson = JsonConvert.SerializeObject(data);
+
+                txtResult.Text = strResultJson;
+            }
+             
+        }
+
+    }
+
+    class jsonData
+    {
+        public logs logs;
+        public string account { get; set; }
+        
+    }
+
+    class logs
+    {
+        public string events { get; set; }
+        public string time { get; set; }
+    }
+}
